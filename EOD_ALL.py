50dd98c7b0ecfce2204a59ed0bbdd935c25a0c2d
businessfoxnox-coder <business.foxnox@gmail.com>
2026-07-29 18:23:07 +0530

    Update EOD_ALL.py

--- EOD_ALL.py
+++ EOD_ALL.py
@@ -331,6 +331,15 @@ try:
     if os.path.exists(SLIDES_DIR):
         shutil.rmtree(SLIDES_DIR)
     os.makedirs(SLIDES_DIR, exist_ok=True)
+    for filename in os.listdir(SLIDES_DIR):
+        file_path = os.path.join(SLIDES_DIR, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print(f"Failed to delete {file_path}. Reason: {e}")
 
     slide_counter = 1
 
